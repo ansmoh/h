ed8efbaa62fcfd8e11c093d0156696db1299037f
Aron Carroll <self@aroncarroll.com>
2015-02-13 11:29:55 +0100

    Include the claim module in the config file

--- h/__init__.py
+++ h/__init__.py
@@ -8,11 +8,6 @@ __all__ = ['main']
 
 
 def includeme(config):
-    config.include('pyramid_jinja2')
-    config.add_jinja2_renderer('.js')
-    config.add_jinja2_renderer('.txt')
-    config.add_jinja2_renderer('.html')
-
     config.include('h.authentication')
     config.include('h.authorization')
     config.include('h.features')
@@ -29,4 +24,7 @@ def includeme(config):
     if config.registry.feature('notification'):
         config.include('h.notification')
 
-    config.include('h.claim')
+    config.include('pyramid_jinja2')
+    config.add_jinja2_renderer('.js')
+    config.add_jinja2_renderer('.txt')
+    config.add_jinja2_renderer('.html')
